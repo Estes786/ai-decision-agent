a65e34542be418b0eca17962635c9c55f9646751
Estes786 <elmmatador0197@gmail.com>
2025-06-26 23:27:44 +0700

    Remove BOM from index.js

--- index.js
+++
@@ -1,9 +0,0 @@
-document.addEventListener('DOMContentLoaded', () => {
-    console.log('Website Portfolio berhasil dimuat sepenuhnya!');
-    const anchors = document.querySelectorAll('a[href="#"]');
-    anchors.forEach(anchor => {
-        anchor.addEventListener('click', function(e) {
-            e.preventDefault();
-        });
-    });
-});
